d8112a45e58c32e9b2e62efee3670fee15b6b637
Thomas Tahmassebi <tjtahmas@gmail.com>
2021-10-19 13:40:22 -0400

    YOUTUBE FETCH

--- script.js
+++ script.js
@@ -36,24 +36,21 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + 
 
 
 
-
-
-
-// var playlistID = 'PLWyCiVKEayX_WuoYgy0eopWvDzCnmoAGn';
-// var YouTubeAPIkey = 'AIzaSyAmz5aARCdvb9Ri8Lw1Bc0yv-XI0JnyH8I';
-
-
-// fetch('https://www.googleapis.com/youtube/v3/playlists?id=' + playlistID + '&key=' + YouTubeAPIkey, {
-//     // The browser fetches the resource from the remote server without first looking in the cache.
-//     // The browser will then update the cache with the downloaded resource.
-//     cache: 'reload',
-// })
-//     .then(function (response) {
-//         return response.json();
-//     })
-//     .then(function (data) {
-//         console.log(data);
-//     });
-
 });
+
+var YouTubeAPIkey = 'AIzaSyAmz5aARCdvb9Ri8Lw1Bc0yv-XI0JnyH8I';
+var searchCritera = 'Rainy%20Day%20Music';
+var searchType = 'playlist'
+var maxResults = 15
+fetch('https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=' + maxResults + '&q=' + searchCritera + '&type=' + searchType + '&key=' + YouTubeAPIkey, {
+    // The browser fetches the resource from the remote server without first looking in the cache.
+    // The browser will then update the cache with the downloaded resource.
+    cache: 'reload',
+})
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
 
